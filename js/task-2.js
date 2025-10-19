e7d01fa338cf852f60a3819eeb287c4fbe268149
Roman Hrydin <romario198901@gmail.com>
2025-10-19 16:03:48 +0300

    complete tasks from hw

---
+++ js/task-2.js
@@ -0,0 +1,27 @@
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    if (this.#items.includes(itemToRemove)) {
+      const index = this.#items.indexOf(itemToRemove);
+      this.#items.splice(index, 1);
+    }
+  }
+}
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems());
+storage.addItem("Droid");
+console.log(storage.getItems());
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems());
+storage.removeItem("Scaner");
+console.log(storage.getItems());
