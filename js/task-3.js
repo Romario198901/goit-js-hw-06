97905863392d90c24cec4a5df52135a6cc961700
Roman Hrydin <romario198901@gmail.com>
2025-10-19 16:05:14 +0300

    correct task3

--- js/task-3.js
+++ js/task-3.js
@@ -7,13 +7,13 @@ class StringBuilder {
     return this.#value;
   }
   padEnd(str) {
-    this.#value+=str;
+    this.#value += str;
   }
   padStart(str) {
-   this.#value = str + this.#value;
+    this.#value = str + this.#value;
   }
   padBoth(str) {
- this.#value = str + this.#value + str;
+    this.#value = str + this.#value + str;
   }
 }
 const builder = new StringBuilder(".");
